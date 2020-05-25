678daadfcf335455fc981281ac59736c038d80cb
Kmbariah <Kmbaria@github.com>
2020-05-25 19:27:23 +0300

    updated the README.md

--- imagegallery/imagegallery/urls.py
+++ imagegallery/imagegallery/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
+    path('',views.imagedisplay)
 ]
+
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
