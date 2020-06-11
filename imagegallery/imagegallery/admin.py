dcbaa0f8ed3644c56095cdf1b66ffb5b4bf1103d
Kmbariah <Kmbaria@github.com>
2020-06-11 10:56:50 +0300

    added new

--- imagegallery/imagegallery/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from imagegallery.models import imggal
-
-admin.site.register(imggal)
--- imagegallery/imagegallery/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from imagegallery.models import imggal
-
-def imagedisplay(request):
-    resultsdisplay=imggal.objects.all()
-    return render(request, 'index.html',{'imggal':resultsdisplay})
